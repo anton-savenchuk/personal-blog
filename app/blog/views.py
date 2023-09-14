56002528b73922ac3f9aa35f4cf5ee8766196d75
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2023-09-14 14:37:32 +0700

    Added class-based views, posts output to template

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+menu = [
+    {"title": "/Все посты", "url_name": "home"},
+    {"title": "/О сайте", "url_name": "home"},
+    {"title": "/Обратная связь", "url_name": "home"},
+]
+
+
+class PostListView(ListView):
+    """Post model view."""
+
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_context_data(self, **kwargs):
+        """Return a dictionary to use as a template context."""
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Все посты"
+        context["menu"] = menu
+
+        return context
+
+    def get_queryset(self):
+        """Return the list of items for this view."""
+        return Post.objects.filter(is_published=True)
