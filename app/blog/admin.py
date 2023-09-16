cb6e167fa49c1c6b8089ee2108a4f75ce9f46b71
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2023-09-16 14:07:22 +0700

    Added ModelAdmin to the admin panel to display posts and categories

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 
 from .models import Category, Post
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class PostAdmin(admin.ModelAdmin):
+    """Post model in the admin panel."""
+
+    list_display = ("id", "title", "time_create", "thumbnail", "is_published")
+    list_display_links = ("id", "title")
+    list_editable = ("is_published",)
+    list_filter = ("is_published", "time_create", "category")
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Category model in the admin panel."""
+
+    list_display = ("id", "title")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
