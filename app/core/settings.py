01b8253363f553e80b28dc8b1bd35544954f5dc1
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2023-10-17 12:10:41 +0700

    Added PostgreSQL

--- app/core/settings.py
+++ app/core/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRESQL_DB"),
+        "USER": env("POSTGRESQL_USER"),
+        "PASSWORD": env("POSTGRESQL_PASSWORD"),
+        "HOST": env("POSTGRESQL_HOST"),
+        "PORT": env("POSTGRESQL_PORT"),
+    },
 }
 
 
